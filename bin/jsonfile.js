b22fdbdd8faec78a84fa0d05e5188248bfe09848
sudha <sudhapackiyam.sureshkumar@kaaviansys.com>
2022-12-11 13:41:00 +0530

    creating new folder..

--- bin/jsonfile.js
+++ bin/jsonfile.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 const fs = require("fs");
+
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -8,31 +9,79 @@ const readline = require("readline").createInterface({
 
 readline.question("Do you want to install json file?", (data) => {
   if (data === "yes") {
-    const file =
-      "{\n" +
-      '\t"spec_dir": "spec",\n' +
-      '\t"spec_files": [\n' +
-      '\t\t"**/*[sS]pec.?(m)js",\n' +
-      '\t\t"!**/*nospec.js"\n' +
-      "\t],\n" +
-      '\t"helpers": [\n' +
-      '\t\t"helpers/**/*.js"\n' +
-      "\t],\n" +
-      '\t"env": {\n' +
-      '\t\t"failSpecWithNoExpectations": false,\n' +
-      '\t\t"stopSpecOnExpectationFailure": false,\n' +
-      '\t\t"stopOnSpecFailure": false,\n' +
-      '\t\t"random": false\n' +
-      "\t}\n" +
-      "}";
-    fs.writeFileSync("spec/support/ksfile.json", file);
-  } else if (data === "no") {
-    readline.close();
-  }
-  // let data =
-  //   "This is a file containing a collection" +
-  //   " of programming languages.\n" +
-  //   "1. C\n2. C++\n3. Python";
+    const path = "./spec/support";
+
+    fs.access(path, (error) => {
+      if (error) {
+        fs.mkdir(path, { recursive: true }, (error) => {
+          if (error) {
+            console.log(error);
+          } else {
+            const file =
+							"{\n" +
+							'\t"spec_dir": "spec",\n' +
+							'\t"spec_files": [\n' +
+							'\t\t"**/*[sS]pec.?(m)js",\n' +
+							'\t\t"!**/*nospec.js"\n' +
+							"\t],\n" +
+							'\t"helpers": [\n' +
+							'\t\t"helpers/**/*.js"\n' +
+							"\t],\n" +
+							'\t"env": {\n' +
+							'\t\t"failSpecWithNoExpectations": false,\n' +
+							'\t\t"stopSpecOnExpectationFailure": false,\n' +
+							'\t\t"stopOnSpecFailure": false,\n' +
+							'\t\t"random": false\n' +
+							"\t}\n" +
+							"}";
+						fs.writeFileSync("spec/support/jasmine.json", file);
+						console.log('File Created !!');
+          }
+        });
+      } else {
+        console.log("Given Directory already exists and created !!");
+				const file =
+							"{\n" +
+							'\t"spec_dir": "spec",\n' +
+							'\t"spec_files": [\n' +
+							'\t\t"**/*[sS]pec.?(m)js",\n' +
+							'\t\t"!**/*nospec.js"\n' +
+							"\t],\n" +
+							'\t"helpers": [\n' +
+							'\t\t"helpers/**/*.js"\n' +
+							"\t],\n" +
+							'\t"env": {\n' +
+							'\t\t"failSpecWithNoExpectations": false,\n' +
+							'\t\t"stopSpecOnExpectationFailure": false,\n' +
+							'\t\t"stopOnSpecFailure": false,\n' +
+							'\t\t"random": false\n' +
+							"\t}\n" +
+							"}";
+						fs.writeFileSync("spec/support/jasmine.json", file);
+      }
+    });
+	} else if (data === 'no') {
+		readline.close();
+	} else {
+		readline.close();
+	}
 
   readline.close();
 });
+
+
+// const path = './spe/supp';
+// fs.access(path, (err) => {
+// 	if(err) {
+// 		console.log('err');
+// 		fs.mkdir(path, {recursive: true}, (error) => {
+// 			if(error) {
+// 				console.log('error');
+// 			} else {
+// 				console.log('done');
+// 			}
+// 		})
+// 	} else {
+// 		console.log('success');
+// 	}
+// });
